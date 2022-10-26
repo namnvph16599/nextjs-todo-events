f3e3e310124f5d47625f7399961fb13486645f35
Ngô Vưn Nam <anh02284@gmail.com>
2022-10-26 21:59:41 +0700

    80: page events

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import EventList from '../../components/events/EventList'
+import { getAllEvents } from '../../DATA'
 
 const EventsPage = () => {
-    return (
-        <div>EventsPage</div>
-    )
+    const events = getAllEvents()
+    return <EventList events={events} />
 }
 
 export default EventsPage
