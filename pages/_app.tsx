0842012bed2236b2816bf0529132e2959ffbfd54
Ngô Vưn Nam <anh02284@gmail.com>
2022-12-30 23:56:06 +0700

    config head inital

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Fragment } from 'react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Fragment>
+    <Head>
+      <title>My Event App</title>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
+    </Head>
+    <Component {...pageProps} />
+  </Fragment>
 }
 
 export default MyApp
